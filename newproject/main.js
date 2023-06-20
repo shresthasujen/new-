248558685e47314af9b2f0cceb49fdc40bf8466f
Ronesh <nakarmi.ronesh@gmail.com>
2023-06-20 11:51:43 +0545

    new onw

--- newproject/main.js
+++ newproject/main.js
@@ -1,13 +1,48 @@
 window.onload = function () {
-    const elements = document.querySelectorAll(".animate");
-  
-    let delay = 0;
-    elements.forEach(function (element) {
-      setTimeout(function () {
-        element.style.opacity = "1";
-        element.style.transform = "translateY(0)";
-      }, delay);
-      delay += 1000;
-    });
-  };
-  
+  const elements = document.querySelectorAll(".animate");
+
+  let delay = 0;
+  elements.forEach(function (element) {
+    setTimeout(function () {
+      element.style.opacity = "1";
+      element.style.transform = "translateY(0)";
+    }, delay);
+    delay += 5000;
+  });
+};
+
+const carousel = document.querySelector(".carousel"),
+  firstImg = carousel.querySelectorAll("img")[0],
+  arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
+const showHideIcons = () => {
+  // showing and hiding prev/next icon according to carousel scroll left value
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? "none" : "block";
+};
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+  });
+});
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
